37801effdf45e8761b71a76237df75aab2dcb957
ziadtarek12 <zi3dtarek@gmail.com>
2025-03-28 11:29:02 +0200

    Added graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,10 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"log"
-	"net/http"
 	"os"
-	"strconv"
 	"time"
 
 	// "filmapi.zeyadtarek.net/internals/cert"
@@ -70,50 +67,11 @@ func main() {
 
     app.models = models.New(db)
 
-    // tlsConfig := &tls.Config{
-    //     CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
-    //     MinVersion:       tls.VersionTLS12,
-    //     MaxVersion:       tls.VersionTLS12,
-    //     CipherSuites: []uint16{
-    //         tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-    //         tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-    //         tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-    //         tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-    //         tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-    //         tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-    //     },
-    // }
-
-    // _, err := os.Stat(cert.CertPath)
-    // if err != nil {
-    //     if os.IsNotExist(err) {
-    //         err = cert.GenerateTLSCertAndKey(cert.CertPath, cert.KeyPath)
-    //         if err != nil {
-    //             app.errorLogger.Fatal(err)
-    //         }
-    //     } else {
-    //         app.errorLogger.Fatal(err)
-    //     }
-    // }
-
-    srv := http.Server{
-        Addr:         ":"+ strconv.Itoa(cfg.port),
-        Handler:      app.routes(),
-        ErrorLog: log.New(logger, "", 0),
-        // TLSConfig:    tlsConfig,
-        ReadTimeout:  10 * time.Second,
-        WriteTimeout: 30 * time.Second,
-        IdleTimeout:  time.Minute,
+    err = app.serve()
+    if err != nil{
+        app.logger.PrintFatal(err, nil)
     }
-
-    app.logger.PrintInfo("starting server", map[string]string{
-        "addr": srv.Addr,
-        "env": cfg.env,
-    })
-
-    // err = srv.ListenAndServeTLS(cert.CertPath, cert.KeyPath)
-    err = srv.ListenAndServe()
-    app.logger.PrintFatal(err, nil)
+   
 }
 
 func openDB(cfg config) (*sql.DB, error){
@@ -130,7 +88,7 @@ func openDB(cfg config) (*sql.DB, error){
         return nil, err
     }
 
-    db.SetConnMaxIdleTime(duration)
+        db.SetConnMaxIdleTime(duration)
     
     ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
     defer cancel()
