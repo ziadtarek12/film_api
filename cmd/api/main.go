08cda7abf1ff54700e42004be60932ae652ac2e0
ziadtarek12 <zi3dtarek@gmail.com>
2025-04-02 12:37:55 +0200

    Added CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,102 +1,113 @@
 package main
 
 import (
-	// "crypto/tls"
 	"context"
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"time"
 
-	// "filmapi.zeyadtarek.net/internals/cert"
 	"filmapi.zeyadtarek.net/internals/jsonlog"
 	"filmapi.zeyadtarek.net/internals/models"
 	_ "github.com/lib/pq"
 )
 
 type config struct {
-    port int
-    env  string
-    db struct{
-        dsn string
-        maxOpenConns int
-        maxIdleConns int
-        maxIdleTime  string
-    }
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 
-    limiter struct{
-        rps     float64
-        burst   int
-        enabled bool
-    }
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
+
+	cors struct {
+		trustedOrigins []string
+		enabled        bool
+	}
 }
 
 type application struct {
-    logger      *jsonlog.Logger
-    config      config
-    models models.Models
+	logger *jsonlog.Logger
+	config config
+	models models.Models
 }
 
 func main() {
-    var cfg config
-    flag.IntVar(&cfg.port, "port", 4000, "API server port")
-    flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
-    flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("FILMAPI_DB_DSN"), "PostgreSQL DSN")
-    flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-    flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
-    flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
-    flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
-    flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum requests per second")
-    flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enabled rate limiter")
-    flag.Parse()
-   
-    logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("FILMAPI_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum requests per second")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enabled rate limiter")
 
-    
-    app := &application{
-        config:      cfg,
-        logger: logger,
-    }
+	// Use flag.Func to parse comma-separated origins
+	flag.Func("cors-trusted-origin", "CORS trusted origins (comma-separated)", func(value string) error {
+		cfg.cors.trustedOrigins = strings.Fields(value)
+		return nil
+	})
 
-    db, err := openDB(cfg)
-    if err != nil{
-        app.logger.PrintFatal(err, nil)
-    }
-    app.logger.PrintInfo("database connection established", nil)
-    defer db.Close()
+	flag.BoolVar(&cfg.cors.enabled, "cors-enabled", true, "Enable CORS")
+	flag.Parse()
 
-    app.models = models.New(db)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
-    err = app.serve()
-    if err != nil{
-        app.logger.PrintFatal(err, nil)
-    }
-   
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	db, err := openDB(cfg)
+	if err != nil {
+		app.logger.PrintFatal(err, nil)
+	}
+	app.logger.PrintInfo("database connection established", nil)
+	defer db.Close()
+
+	app.models = models.New(db)
+
+	err = app.serve()
+	if err != nil {
+		app.logger.PrintFatal(err, nil)
+	}
+
 }
 
-func openDB(cfg config) (*sql.DB, error){
-    db, err := sql.Open("postgres", cfg.db.dsn)
-    if err != nil{
-        return nil, err
-    }
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
 
-    db.SetMaxOpenConns(cfg.db.maxOpenConns)
-    db.SetMaxIdleConns(cfg.db.maxIdleConns)
-    
-    duration, err := time.ParseDuration(cfg.db.maxIdleTime)
-    if err != nil{
-        return nil, err
-    }
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 
-        db.SetConnMaxIdleTime(duration)
-    
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
 
-    err = db.PingContext(ctx)
-    if err != nil{
-        return nil, err
-    }
+	db.SetConnMaxIdleTime(duration)
 
-    return db, nil
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -14,8 +14,6 @@ import (
 	"golang.org/x/time/rate"
 )
 
-
-
 func (app *application) chainMiddleware(mux http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
 	if len(middleware) == 0 {
 		return mux
@@ -31,10 +29,10 @@ func (app *application) chainMiddleware(mux http.Handler, middleware ...func(htt
 	return handler
 }
 
-func (app *application) recoverPanic(next http.Handler) http.Handler{
+func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func(){
-			if err := recover(); err != nil{
+		defer func() {
+			if err := recover(); err != nil {
 				w.Header().Set("Connection", "close")
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
@@ -43,7 +41,6 @@ func (app *application) recoverPanic(next http.Handler) http.Handler{
 		next.ServeHTTP(w, r)
 	})
 }
-
 
 func (app *application) authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -62,7 +59,7 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 		}
 
 		token := headerParts[1]
-		
+
 		v := validator.New()
 
 		if models.ValidateTokenPlaintext(v, token); !v.Valid() {
@@ -86,29 +83,28 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) rateLimit(next http.Handler) http.Handler {
 
-func (app *application) rateLimit(next http.Handler) http.Handler{
-
-	if !app.config.limiter.enabled{
+	if !app.config.limiter.enabled {
 		return next
 	}
-	
-	type client struct{
-		limiter *rate.Limiter
+
+	type client struct {
+		limiter  *rate.Limiter
 		lastSeen time.Time
 	}
 
 	var (
-		mu sync.Mutex
+		mu      sync.Mutex
 		clients = make(map[string]*client)
 	)
 
-	go func ()  {
+	go func() {
 		for {
 			time.Sleep(time.Minute)
 			mu.Lock()
 			for ip, client := range clients {
-				if time.Since(client.lastSeen) > 3 * time.Minute {
+				if time.Since(client.lastSeen) > 3*time.Minute {
 					delete(clients, ip)
 				}
 			}
@@ -119,16 +115,16 @@ func (app *application) rateLimit(next http.Handler) http.Handler{
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil{
+		if err != nil {
 			app.serverErrorResponse(w, r, err)
 			return
 		}
 		mu.Lock()
-		if _, found := clients[ip]; !found{
+		if _, found := clients[ip]; !found {
 			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
 		}
 
-		if !clients[ip].limiter.Allow(){
+		if !clients[ip].limiter.Allow() {
 			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
@@ -144,16 +140,15 @@ func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.Han
 		user := app.contextGetUser(r)
 		if user.IsAnonyomous() {
 			app.authenticationRequiredResponse(w, r)
-			return	
+			return
 		}
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (app *application) requireActivatedUser(next http.Handler) http.Handler{
+func (app *application) requireActivatedUser(next http.Handler) http.Handler {
 	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
-
 
 		if !user.Activated {
 			app.inactiveAccountResponse(w, r)
@@ -175,9 +170,37 @@ func (app *application) ensureTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
+		origin := r.Header.Get("Origin")
+
+		if origin != "" && len(app.config.cors.trustedOrigins) != 0 {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
+		}
+
+		w.WriteHeader(http.StatusForbidden)
+	})
+}
 
 func (app *application) requirePermission(code string, next http.Handler) http.Handler {
-	fn := http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
 		permissions, err := app.models.Permissions.GetAllForUser(user.ID)
 		if err != nil {
